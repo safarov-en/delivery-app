ee1dc4ebaefda32352e834e551c3dc7adaa32968
safarov-en <safarov.elkhan110@gmail.com>
2024-05-14 17:31:37 +0500

    Adding a button and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Button from './components/Button/Button'
+import Input from './components/Input/Input'
 
 function App() {
   return (
     <>
       <Button>Кнопка</Button>
+      <Input />
     </>
   )
 }
