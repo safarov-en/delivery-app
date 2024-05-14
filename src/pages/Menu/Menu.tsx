ea1b7ce5d3fe01e8369681472cc2f4d1f06c12c8
safarov-en <safarov.elkhan110@gmail.com>
2024-05-14 21:31:34 +0500

    Adding a card

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import Headling from "../../Headling/Headling";
+import ProductCard from "../../components/ProductCard/ProductCard";
 import Search from "../../components/Search/Search";
 import styles from './Menu.module.css'
 
@@ -7,6 +8,16 @@ export function Menu() {
         <div className={styles['head']}>
             <Headling>Меню</Headling>
             <Search placeholder='Введите блюдо или состав' />
+        </div>
+        <div>
+            <ProductCard
+                id={1}
+                title="Наслаждение"
+                description='Руккола, помидоры, оливки'
+                rating={4.5}
+                price={300}
+                image='/product.png'
+            />
         </div>
     </>
 }
