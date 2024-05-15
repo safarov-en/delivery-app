75f71e583b421afb1959628fb9f501e4efc72039
safarov-en <safarov.elkhan110@gmail.com>
2024-05-15 12:41:30 +0500

    Adding an entry

---
+++ src/layout/Auth/AuthLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import styles from './AuthLayout.module.css'
+
+export function AuthLayout() {
+    return <div className={styles['layout']}>
+        <div className={styles['logo']}>
+            <img src='/logo.svg' />
+        </div>
+        <div className={styles['content']}>
+            <Outlet />
+        </div>
+    </div>
+}
--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,9 @@ import { Layout } from './layout/Menu/Layout.tsx'
 import { Product } from './pages/Product/Product.tsx'
 import axios from 'axios'
 import { PREFIX } from './helpers/API.ts'
+import { AuthLayout } from './layout/Auth/AuthLayout.tsx'
+import { Login } from './pages/Login/Login.tsx'
+import { Register } from './pages/Register/Register.tsx'
 const Menu = lazy(() => import('./pages/Menu/Menu.tsx'))
 
 const router = createBrowserRouter([
@@ -31,6 +34,20 @@ const router = createBrowserRouter([
             data: await axios.get(`${PREFIX}/products/${params.id}`).then(data => data)
           })
         }
+      }
+    ]
+  },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'register',
+        element: <Register />
       }
     ]
   },
