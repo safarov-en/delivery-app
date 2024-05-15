3b2d28fc457aba0636c7a3c7cb5a4437c41e43dd
safarov-en <safarov.elkhan110@gmail.com>
2024-05-15 17:09:34 +0500

    Adding a reducer

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,8 @@ import { AuthLayout } from './layout/Auth/AuthLayout.tsx'
 import { Login } from './pages/Login/Login.tsx'
 import { Register } from './pages/Register/Register.tsx'
 import { RequireAuth } from './helpers/RequireAuth.tsx'
+import { Provider } from 'react-redux'
+import { store } from './store/store.ts'
 const Menu = lazy(() => import('./pages/Menu/Menu.tsx'))
 
 const router = createBrowserRouter([
@@ -60,6 +62,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
