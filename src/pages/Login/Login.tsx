65a53b4760bbe55d9958773086384e6b76f58864
safarov-en <safarov.elkhan110@gmail.com>
2024-05-15 14:51:26 +0500

    Registration request

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,22 +3,51 @@ import Headling from "../../Headling/Headling";
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
 import styles from './Login.module.css'
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
+import axios, { AxiosError } from "axios";
+import { PREFIX } from "../../helpers/API";
+
+export type LoginForm = {
+    email: {
+        value: string
+    }
+    password: {
+        value: string
+    }
+}
 
 export function Login() {
-    const submit = (e: FormEvent) => {
+    const [error, setError] = useState<string | null>()
+    const submit = async (e: FormEvent) => {
         e.preventDefault()
+        setError(null)
+        const target = e.target as typeof e.target & LoginForm
+        const {email, password} = target
+        await sendLogin(email.value, password.value)
     }
-    return <div className={styles['login']} onSubmit={submit}>
+    const sendLogin = async (email: string, password: string) => {
+        try {
+            const {data} = await axios.post(`${PREFIX}/auth/login`, {
+                email,
+                password
+            })
+        } catch(e) {
+            if(e instanceof AxiosError) {
+                setError(e.response?.data.message)
+            }
+        }
+    }
+    return <div className={styles['login']}>
         <Headling>Вход</Headling>
-        <form className={styles['form']}>
+        {error && <div className={styles['error']}>{error}</div>}
+        <form className={styles['form']} onSubmit={submit}>
             <div className={styles['field']}>
                 <label htmlFor="email">Ваш email</label>
-                <Input id="email" placeholder="Email" />
+                <Input id="email" name='email' placeholder="Email" />
             </div>
             <div className={styles['field']}>
                 <label htmlFor="password">Ваш пароль</label>
-                <Input id="password" type="password" placeholder="Пароль" />
+                <Input id="password" name='password' type="password" placeholder="Пароль" />
             </div>
             <Button appearence="big">Вход</Button>
         </form>
